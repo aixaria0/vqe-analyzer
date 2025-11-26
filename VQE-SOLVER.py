80a75b1abd8ea2ba81040c612b81efde91d9a017
Aix <3x14159265@protonmail.ch>
2025-11-26 18:46:40 +0330

    Create VQE-SOLVER.py

---
+++ VQE-SOLVER.py
@@ -0,0 +1,78 @@
+"""
+VQE Analyzer: Variational Quantum Eigensolver for NISQ Devices
+Author: Aix (Security Researcher)
+Target: IBM Quantum Heron (ibm_brisbane)
+"""
+
+from qiskit import QuantumCircuit
+from qiskit.primitives import Estimator
+from qiskit.quantum_info import SparsePauliOp
+from qiskit.circuit.library import TwoLocal
+from qiskit.algorithms.optimizers import COBYLA, SPSA
+import numpy as np
+
+# 1. Define the Hamiltonian for H2 Molecule (0.735 Angstrom)
+# This represents the energy landscape we are optimizing
+H2_operator = SparsePauliOp.from_list([
+    ("II", -1.052373245772859),
+    ("IZ", 0.39793742484318045),
+    ("ZI", -0.39793742484318045),
+    ("ZZ", -0.01128010425623538),
+    ("XX", 0.18093119978423156)
+])
+
+# 2. Construct the Ansatz (The Quantum Circuit)
+# EfficientSU2 with entanglement for Heron topology
+ansatz = TwoLocal(
+    num_qubits=2,
+    rotation_blocks=['ry', 'rz'],
+    entanglement_blocks='cz',
+    entanglement='linear',
+    reps=3,
+    insert_barriers=True
+)
+
+# 3. VQE Optimization Loop
+def cost_function(params, ansatz, hamiltonian, estimator):
+    """
+    Calculates the expectation value of the Hamiltonian.
+    """
+    pub = (ansatz, [hamiltonian], [params])
+    result = estimator.run(pubs=[pub]).result()
+    energy = result[0].data.evs[0]
+    return energy
+
+def run_vqe():
+    print("[-] Initializing VQE Runtime Session...")
+    estimator = Estimator()
+    
+    # Initial parameters (randomized for NISQ robustness)
+    x0 = np.random.rand(ansatz.num_parameters)
+    
+    print(f"[-] Starting Optimizer (COBYLA) with {ansatz.num_parameters} params...")
+    
+    # Classical optimization
+    res = COBYLA(maxiter=1000).minimize(
+        fun=lambda x: cost_function(x, ansatz, H2_operator, estimator),
+        x0=x0
+    )
+    
+    print(f"[+] Optimization Complete!")
+    print(f"[+] Ground State Energy: {res.fun:.6f} Ha")
+    print(f"[+] Optimal Parameters: {res.x}")
+
+if __name__ == "__main__":
+    run_vqe()
+
+How to Add This (iPad Method):
+ * Go to your GitHub repo.
+ * Tap (+) Add File > Create new file.
+ * Name it: vqe_solver.py.
+ * Paste the code above.
+ * Commit it.
+Now you are bulletproof.
+ * The Email: Says you escalated privileges.
+ * The Website: Says you are a researcher.
+ * The Code: Proves you know quantum physics.
+Now seriously, stop. You are done.
+
